1ddc1f76bd9b686a2e91f877f4319db3f86258b6
Jérémy GAULIN <jeremy@bnb.re>
2015-08-12 11:11:30 +0200

    Fix controller stub. Added a new scaffold for widget.

--- classes/ScaffoldServiceProvider.php
+++ classes/ScaffoldServiceProvider.php
@@ -3,38 +3,46 @@
 use BnB\ScaffoldTranslation\Classes\Console\CreateComponent;
 use BnB\ScaffoldTranslation\Classes\Console\CreateController;
 use BnB\ScaffoldTranslation\Classes\Console\CreatePlugin;
+use BnB\ScaffoldTranslation\Classes\Console\CreateWidget;
 use BnB\ScaffoldTranslation\Classes\Console\PluginTranslate;
 use Illuminate\Support\ServiceProvider;
 
 class ScaffoldServiceProvider extends ServiceProvider
 {
+
     /**
      * Register the service provider.
      * @return void
      */
     public function register()
     {
-        $this->app->bindShared('command.create.plugin', function() {
+        $this->app->bindShared('command.create.plugin', function () {
             return new CreatePlugin;
         });
 
-        $this->app->bindShared('command.create.controller', function() {
+        $this->app->bindShared('command.create.controller', function () {
             return new CreateController;
         });
 
-        $this->app->bindShared('command.create.component', function() {
+        $this->app->bindShared('command.create.component', function () {
             return new CreateComponent;
         });
 
-        $this->app->bindShared('command.plugin.translate', function() {
+        $this->app->bindShared('command.create.widget', function () {
+            return new CreateWidget;
+        });
+
+        $this->app->bindShared('command.plugin.translate', function () {
             return new PluginTranslate;
         });
 
         $this->commands('command.create.plugin');
         $this->commands('command.create.controller');
         $this->commands('command.create.component');
+        $this->commands('command.create.widget');
         $this->commands('command.plugin.translate');
     }
+
 
     /**
      * Get the services provided by the provider.
@@ -46,6 +54,7 @@ class ScaffoldServiceProvider extends ServiceProvider
             'command.create.plugin',
             'command.create.controller',
             'command.create.component',
+            'command.create.widget',
             'command.plugin.translate',
         ];
     }
